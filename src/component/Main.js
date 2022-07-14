dcb8ce8cfc3953f9218ef578af47eadb768e6d83
alilotfi.dev <lotfi26.ali@gmail.com>
2022-07-14 18:01:31 +0430

    misspelling

--- src/component/Main.js
+++ src/component/Main.js
@@ -6,12 +6,11 @@ function Main() {
       <h2 className='hi'>hi there,</h2>
       <p className='about-me'>
         This is <b>Ali</b>. I am a 18 year old self-taught{' '}
-        <b>fontend developer</b> based in <b>Neyshabour, Iran</b>.Since I was a
-        child, I have been very interested in computers. When I wanted to start
-        a career, I thought, what better job than what I'm passionate about?
-        This is how I decided to learn web development and now I am a front-end
-        developer. I also love photography and music. If you would like to work
-        with me,{' '}
+        <b>front-end developer</b> based in <b>Neyshabour, Iran</b>.Since I was
+        a child, I have been very interested in computers. When I wanted to
+        start a career, I thought, what better job than what I'm passionate
+        about? This is how I decided to learn web development and now I am a
+        front-end developer. If you would like to work with me,{' '}
         <b>
           <a href='mailto:lotfi26.ali@gmail.com'>drop me an email</a>
         </b>
