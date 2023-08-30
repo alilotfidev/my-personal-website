9b88b3a2ae8cd38658d4caffa2a4a742464fc773
alilotfi.dev <lotfi26.ali@gmail.com>
2023-08-30 16:16:05 +0330

    portfolio data added

---
+++ data/PortfolioData.js
@@ -0,0 +1,45 @@
+const PortfolioData = [
+  {
+    id: "1",
+    name: "My Website",
+    technologies: ["React.js", "Tailwind CSS"],
+    link: "https://alilotfi.netlify.app/",
+    github: "https://github.com/alilotfidev/my-personal-website",
+  },
+  {
+    id: "2",
+    name: "E-coomerce Website",
+    technologies: ["React.js", "Commerce.js API"],
+    link: "https://unique-daifuku-8d1362.netlify.app/",
+    github: "https://github.com/alilotfidev/bike-shop",
+  },
+  {
+    id: "3",
+    name: "Movie Library with Next.js",
+    technologies: ["Next.js", "Tailwind CSS"],
+    link: "https://next-movie-library-17xi.vercel.app/",
+    github: "https://github.com/alilotfidev/next-movie-library",
+  },
+  {
+    id: "4",
+    name: "Movie Library",
+    technologies: ["JavaScript", "Webpack"],
+    link: "https://elegant-lollipop-f43c25.netlify.app/",
+    github: "https://github.com/alilotfidev/my-movie-library",
+  },
+  {
+    id: "5",
+    name: "Portfolio Website Template",
+    technologies: ["React.js", "Tailwind CSS"],
+    link: "https://portfolio-template-w-tailwind-react.netlify.app/",
+    github: "",
+  },
+  {
+    id: "6",
+    name: "A few small projects on my Codepen page",
+    technologies: ["HTML", "CSS"],
+    link: "https://codepen.io/alilotfidev",
+    github: "",
+  },
+];
+export default PortfolioData;
