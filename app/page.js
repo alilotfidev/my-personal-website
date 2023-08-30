42ec94d6a9293870a50958f02f95b6719672b74a
alilotfi.dev <lotfi26.ali@gmail.com>
2023-08-30 16:16:48 +0330

    adding components to the page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
+import Info from "@/components/Info";
+import Links from "@/components/Links";
+import PortfolioList from "@/components/PortfolioList";
+import Profile from "@/components/Profile";
+import WorkExperience from "@/components/WorkExperience";
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className="flex min-h-screen flex-col gap-y-14 p-8 md:max-w-[80vw] md:p-16 lg:max-w-[70vw] lg:p-24 xl:max-w-[45vw]">
+      <Profile />
+      <Info />
+      <WorkExperience />
+      <PortfolioList />
+      <Links />
+    </main>
   );
 }
