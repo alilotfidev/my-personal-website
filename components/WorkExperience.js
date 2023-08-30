5e14ba74b32a73e0c4f8d3e6a078813512841785
alilotfi.dev <lotfi26.ali@gmail.com>
2023-08-30 16:15:07 +0330

    work experience component done

---
+++ components/WorkExperience.js
@@ -0,0 +1,43 @@
+import MyLink from "./ui/MyLink";
+
+export default function WorkExperience() {
+  const experiences = [
+    {
+      role: "Junior Front-end Developer",
+      company: "Astroshot Media",
+      companyLink: "https://astroshotmedia.com",
+      time: "2023 - present",
+      technologies: ["JS", "Next", "React", "TailwindCSS", "Contentful"],
+    },
+    {
+      role: "Junior Front-end Developer",
+      company: "Fateme Mohiti",
+      companyLink: "https://fatememohiti.ir",
+      time: "2023 - 2023",
+      technologies: ["JS", "Next", "React", "TailwindCSS", "Netlify"],
+    },
+  ];
+
+  return (
+    <div className="WorkExperience">
+      <h3>Work Experience</h3>
+      <div className="experiences">
+        {experiences.map((item) => (
+          <div className="mt-6 flex flex-col gap-y-2 text-sm sm:grid sm:grid-cols-4">
+            <div className="year secondary-text">{item.time}</div>
+            <div className="col-span-3">
+              <h4 className="text-base">
+                <MyLink href={item.companyLink}>
+                  {item.role} - {item.company}
+                </MyLink>
+              </h4>
+              <p className="secondary-text mt-2 text-xs">
+                {item.technologies.join(" - ")}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
